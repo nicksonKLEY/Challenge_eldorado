d0b0ffcef4d94886e5ecf3a6bc399544fdbed07c
nicksonKLEY <nicksonkley14@gmail.com>
2021-08-29 15:25:46 -0400

    Removed storyboard

---
+++ Challenge_Eldorado/Challenge_Eldorado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge_Eldorado
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 29/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Challenge_Eldorado/Challenge_EldoradoTests/Challenge_EldoradoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Challenge_EldoradoTests.swift
+//  Challenge_EldoradoTests
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 29/08/21.
+//
+
+import XCTest
+@testable import Challenge_Eldorado
+
+class Challenge_EldoradoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

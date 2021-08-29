cfb6582c86e600e71bf04a3449aced5193b35f0d
nicksonKLEY <nicksonkley14@gmail.com>
2021-08-29 16:43:47 -0400

    Start codeview: Create segmentedControl

--- Challenge_Eldorado/Challenge_Eldorado/ViewController.swift
+++ Challenge_Eldorado/Challenge_Eldorado/ViewController.swift
@@ -8,6 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let segmentedViews = ["All", "Saved"]
+    
+    override func loadView() {
+        let body = UIView(frame: UIScreen.main.bounds)
+        
+        self.view = body
+        
+        addSegmented()
+        
+    }
+    
+    private func addSegmented(){
+        
+        let segmented = UISegmentedControl(items: segmentedViews)
+        segmented.addTarget(self, action: #selector(segmentedDidChange(_:)), for: .valueChanged)
+        
+        segmented.translatesAutoresizingMaskIntoConstraints = false
+        
+        segmented.selectedSegmentIndex = 0
+        
+        view.addSubview(segmented)
+        
+        NSLayoutConstraint.activate([
+            segmented.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            segmented.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            segmented.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+    
+    @objc func segmentedDidChange(_ segmentedControl: UISegmentedControl){
+        
+        print("\(segmentedViews[segmentedControl.selectedSegmentIndex])")
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

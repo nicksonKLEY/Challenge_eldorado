5b04f63ca354c7677070eb6385ae4c9961ab8e96
nicksonKLEY <nicksonkley14@gmail.com>
2021-08-31 01:01:23 -0400

    Create function to request list of pulls

--- Challenge_Eldorado/Challenge_Eldorado/Scenes/DetailView/ViewModel/RepositoryDetailViewModel.swift
+++ Challenge_Eldorado/Challenge_Eldorado/Scenes/DetailView/ViewModel/RepositoryDetailViewModel.swift
@@ -12,9 +12,32 @@ class RepositoryDetailViewModel {
     let repository: Repository.Item
     let title: String
     
+    var pulls: [Pulls] = []
+    
     init(_ repository: Repository.Item) {
         self.repository = repository
         self.title = repository.name ?? ""
+    }
+    
+    func requestPullsRepository(_ completion: @escaping (Result<[Pulls], Error>)->Void){
+        guard let owner = repository.owner?.login else { return }
+        guard let repositoryName = repository.name else { return }
+        guard let url = URL(string: "http://api.github.com/repos/\(owner)/\(repositoryName)/pulls") else {
+            return
+            
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else { return }
+            guard let pulls = try? JSONDecoder().decode([Pulls].self, from: data) else {
+                return }
+            
+            completion(.success(pulls))
+        }
+        task.resume()
     }
     
 }

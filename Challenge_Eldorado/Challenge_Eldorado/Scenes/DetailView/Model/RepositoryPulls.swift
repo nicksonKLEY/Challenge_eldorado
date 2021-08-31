419e8333aed8af949d4b8259f2b73d3e653d7d45
nicksonKLEY <nicksonkley14@gmail.com>
2021-08-30 23:08:42 -0400

    Create Pull struct to consume a list of pullReuquest

---
+++ Challenge_Eldorado/Challenge_Eldorado/Scenes/DetailView/Model/RepositoryPulls.swift
@@ -0,0 +1,15 @@
+//
+//  RepositoryPulls.swift
+//  Challenge_Eldorado
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 30/08/21.
+//
+
+import Foundation
+
+struct Pulls: Codable {
+    
+    let title: String
+    let number: Int
+    let body: String
+}
